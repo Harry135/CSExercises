1b767ecb4065752324357d029ce2f00328b9d7c1
Harry135 <singing.sagi@gmail.com>
2015-08-16 16:46:35 +0800

    done

--- CSExercises/Ex07-15/Ex12.cs
+++ CSExercises/Ex07-15/Ex12.cs
@@ -10,6 +10,18 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            double D = 0; // The distance travelled in KM
+            double TC = 0;// the total cost
+            double FC = 2.40; // fixed charge
+            double CPKM = 0.40; // cost per KM travelled
+            Console.WriteLine("Enter the distance travelled: ");
+
+            D = double.Parse(Console.ReadLine());
+            Double A = FC + (D * CPKM);
+            TC = Math.Round(A,1);
+            
+            Console.WriteLine("The Total cost for the journey: ${0:#.00} ", TC);
+
             
         }
     }
