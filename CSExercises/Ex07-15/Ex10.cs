d52256a7b1dd7fcd8e1c2fa3d78b485cf40b0ffe
Harry135 <singing.sagi@gmail.com>
2015-08-16 16:40:42 +0800

    done

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,6 +10,24 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            int x1, x2, y1, y2;
+            float D;
+
+            Console.WriteLine("Enter the X coordinate of the first point(X1): ");
+            x1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Y coordinate of the first point(Y1): ");
+            y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the X coordinate of the first point(X2): ");
+            x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Y coordinate of the first point(Y2): ");
+            y2 = int.Parse(Console.ReadLine());
+
+            D = ((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1));
+            Console.WriteLine("The distance between ({0},{1}) and ({2},{3}) is {4}  ",x1, y1, x2, y2,D);
+            
             
         }
     }
--- CSExercises/Ex07-15/Ex11.cs
+++ CSExercises/Ex07-15/Ex11.cs
@@ -10,7 +10,18 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            double D = 0; // The distance travelled in KM
+            double TC = 0;// the total cost
+            double FC = 2.40; // fixed charge
+            double CPKM = 0.40; // cost per KM travelled
+            Console.WriteLine("Enter the distance travelled: ");
             
+            D = double.Parse(Console.ReadLine());
+
+            TC = FC + (D * CPKM);
+            Console.WriteLine("The Total cost for the journey: ${0} ",TC);
+                     
+
         }
     }
 }
