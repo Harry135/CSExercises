fa2130765ac0787220b5b44e913c40b636c34aa2
Harry135 <singing.sagi@gmail.com>
2015-08-16 19:43:58 +0800

    done without actual answer

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -10,7 +10,21 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
-        }
+            double D = 0; // The distance travelled in KM
+            double TC = 0;// the total cost
+            double FC = 2.40; // fixed charge
+            double CPKM = 0.40; // cost per KM travelled
+            double A = 0;
+            double B = 0;
+            int C = 0;
+            Console.WriteLine("Enter the distance travelled: ");
+
+            D = double.Parse(Console.ReadLine());
+            TC = FC + (D * CPKM);
+            Double TC1 = Math.Ceiling(TC);
+                  
+            Console.WriteLine("The Total cost for the journey: ${0:#.00} ", TC1);
+                       
+          }
     }
 }
