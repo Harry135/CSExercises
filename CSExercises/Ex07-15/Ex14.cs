3122c00333f1ccb13608ec1cc69af7ecdfd3c72a
Harry135 <singing.sagi@gmail.com>
2015-08-16 19:01:12 +0800

    done

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,9 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
+            double S, A, B, C, Area, X;
+            
+            Console.WriteLine("Area Of a Triangle");
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Enter the value of A");
+            A = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of B");
+            B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of C");
+            C = double.Parse(Console.ReadLine());
+
+            S = (A + B + C) / 2;
+
+            if(((A+B)>C) && ((B+C)>A) && ((A+C)>B))// triangle in-equality theorem
+            {
+                Area = Math.Sqrt(S * (S - A) * (S - B) * (S - C));
+
+                Console.WriteLine("The Area of the triangle is " + Area);
+
+            }
+            else
+            {
+                Console.WriteLine("The input cannot form a triangle");
+
+            }
+
+
+            
             
         }
     }
--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
+            int number, remainder, sum = 0;
+
+            Console.WriteLine("To find if it is Armstrong Number");
+            Console.WriteLine("----------------------------------");
             
+            Console.WriteLine("Enter a three digit number:");
+            number = int.Parse(Console.ReadLine());
+
+            
+            if ((number < 100) || (number >= 1000))
+            {
+                Console.WriteLine("Please enter a three digit number");
+
+            }
+            else
+            {
+                int A = number;
+                for (int i = 0; i < 3; i++)
+                {
+                    remainder = A % 10;
+                    sum = sum + (remainder * remainder * remainder);
+                    A = A / 10;
+                }
+
+                if (sum == number)
+                {
+                    Console.WriteLine("Entered Number is an Armstrong Number");
+                }
+                else
+                    Console.WriteLine("Entered Number is not an Armstrong Number");
+                
+           }
         }
     }
 }
